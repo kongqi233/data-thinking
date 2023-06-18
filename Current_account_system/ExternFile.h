9dd95fce025f6f00908ad52fab4bdf4a6a22fcfe
有缘 <122378263+YOUYUANZY@users.noreply.github.com>
2023-06-18 15:05:24 +0800

    Update ExternFile.h
    账户格式

--- Current_account_system/ExternFile.h
+++ Current_account_system/ExternFile.h
@@ -3,5 +3,18 @@
 */
 #ifndef EXTERNFILE_H
 #define EXTERNFILE_H
+enum Sex{
+    MALE,FEMALE
+};
+typedef struct{
+    char* name;//名字
+    Sex sex;//性别
+    char* telephone;//手机号
+    short int cardID[4];//卡号
+    char* IDnumber;//身份证号
+    char* password;//密码
+    double balance;//余额
+    short int year,month,day,hour,minute,second;//时间
+} Account;
 void p(void);
 #endif
