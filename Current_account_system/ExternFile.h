838b58fcaf8a519a42ae830eaa839d52378ebbaf
有缘 <122378263+YOUYUANZY@users.noreply.github.com>
2023-06-18 16:20:36 +0800

    Update ExternFile.h
    利率

--- Current_account_system/ExternFile.h
+++ Current_account_system/ExternFile.h
@@ -3,16 +3,17 @@
 */
 #ifndef EXTERNFILE_H
 #define EXTERNFILE_H
+#define INTEREST_RATE 0.011//利率
 enum Sex{
     MALE,FEMALE
 };
 typedef struct{
-    char* name;//名字，最大长度20
+    string name;//名字，最大长度20
     Sex sex;//性别
-    char* telephone;//手机号，长度11
+    string telephone;//手机号，长度11
     short int cardID[4];//卡号，长度16
-    char* IDnumber;//身份证号，长度18
-    char* password;//密码，长度6
+    string IDnumber;//身份证号，长度18
+    string password;//密码，长度6
     double balance;//余额
     short int year,month,day,hour,minute,second;//时间
 } Account;
