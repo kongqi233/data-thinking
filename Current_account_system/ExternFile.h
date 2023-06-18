fec2df1a6e286411d25788bcb79b2100053139a7
有缘 <122378263+YOUYUANZY@users.noreply.github.com>
2023-06-18 16:16:43 +0800

    Update ExternFile.h

--- Current_account_system/ExternFile.h
+++ Current_account_system/ExternFile.h
@@ -7,14 +7,14 @@ enum Sex{
     MALE,FEMALE
 };
 typedef struct{
-    char* name;//名字
+    char* name;//名字，最大长度20
     Sex sex;//性别
-    char* telephone;//手机号
-    short int cardID[4];//卡号
-    char* IDnumber;//身份证号
-    char* password;//密码
+    char* telephone;//手机号，长度11
+    short int cardID[4];//卡号，长度16
+    char* IDnumber;//身份证号，长度18
+    char* password;//密码，长度6
     double balance;//余额
     short int year,month,day,hour,minute,second;//时间
 } Account;
-void p(void);
+typedef 
 #endif
