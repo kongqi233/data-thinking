a18579fcbaf56182e0a7fafbd7b26f1d2444d685
llzbat <93711750+llzbat@users.noreply.github.com>
2023-06-21 20:36:48 +0800

    Update Account_Closure.cpp
    销户

--- Current_account_system/Account_Closure.cpp
+++ Current_account_system/Account_Closure.cpp
@@ -4,3 +4,29 @@
 #include<bits/stdc++.h>
 #include "ExternFile.h"
 using namespace std;
+void Account_Closure(Account &a){
+    char c;
+    string s;
+    cout<<"是否确认销户(Y/N):"<<endl;
+    cin>>c;
+    while(c!='N'&&c!='Y'){
+        cout<<"输入错误，请重新输入:"<<endl;
+        cin>>c;
+    }
+    if(c=='N'){
+
+    }
+    else if(c=='Y'){
+        cout<<"请输入密码验证："<<endl;
+        cin>>s;
+        while(s!=a.password){
+            cout<<"密码错误，请重新输入："<<endl;
+            cin>>s;
+        }
+        cout<<"密码正确，即将销户！"<<endl;
+        _sleep(1000);
+        a.tag=true;
+        cout<<"销户成功！"<<endl;
+    }
+
+}
